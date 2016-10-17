cf4c11ec82cb2b2af76c4a7bbd649a293bb666f3
fefefy <seriousfelix@hotmail.fr>
2016-10-17 16:50:23 +0200

    Small update Add 2 layouts

--- app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
+++ app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
@@ -2,6 +2,9 @@ package projetstl.com.imagerecognizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button libraryButton = (Button)findViewById(R.id.Library_Button);
+
+
+        libraryButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Log.i("Library button","User clicked the library Button");
+                setContentView(R.layout.library_layout);
+                //TODO ajout link vers classe Library.java
+            }
+        });
+
+        Button photoButton = (Button)findViewById(R.id.Photo_button);
+        photoButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Log.i("Photo Button","User clicked the photo Button");
+                setContentView(R.layout.photo_layout);
+
+                //TODO ajout link vers classe Photo.java
+            }
+            });
     }
+
 }
+
---
+++ app/src/main/java/projetstl/com/imagerecognizer/Photo.java
@@ -0,0 +1,8 @@
+package projetstl.com.imagerecognizer;
+
+/**
+ * Created by serio on 17/10/2016.
+ */
+
+public class Photo {
+}
