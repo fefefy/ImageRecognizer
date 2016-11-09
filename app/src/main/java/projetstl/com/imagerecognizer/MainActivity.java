5c5db772d1af9dd0ebf9ddeacc1efdb28b8ae7c6
fefefy <seriousfelix@hotmail.fr>
2016-11-09 15:13:47 +0100

    Update images from gallery

--- app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
+++ app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
@@ -3,30 +3,27 @@ package projetstl.com.imagerecognizer;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.InputStream;
-import java.net.URL;
+import static android.R.attr.data;
+
 
 public class MainActivity extends Activity {
 
 
-    public Gallery gal = new Gallery();
     private static int LOAD_IMAGE = 1;
     private String ImageString;
+    private int READ_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +36,11 @@ public class MainActivity extends Activity {
         galleryButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 Log.i("Gallery button","User clicked the gallery Button");
-                //setContentView(R.layout.gallery_layout);
-
+                //gal.loadImagefromGallery();
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                         MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                String test = gal.getImageString();
-
-
-
-               }
+                startActivityForResult(galleryIntent, LOAD_IMAGE);
+            }
         });
 
         Button photoButton = (Button)findViewById(R.id.Photo_button);
@@ -61,6 +54,38 @@ public class MainActivity extends Activity {
             });
     }
 
+    protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
 
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                ImageString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.Pictures_ImageView);
+
+                //requests permission to read a files from user's device
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_PERMISSION);
+
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory.decodeFile(ImageString));
+
+            } else {
+                Toast.makeText(this, "Vous n'avez pas selectionné d'images !", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Problème détecté", Toast.LENGTH_LONG).show();
+        }
+    }
 }
 
