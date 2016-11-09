829e91db766562c7f7a4e63903d9b8a41436b1c9
meiyoo <sebastien.sekouri@gmail.com>
2016-11-09 16:15:56 +0100

    Photo dans le main

--- app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
+++ app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
@@ -1,91 +1,72 @@
 package projetstl.com.imagerecognizer;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import static android.R.attr.data;
-
+import java.io.File;
 
 public class MainActivity extends Activity {
 
-
     private static int LOAD_IMAGE = 1;
-    private String ImageString;
-    private int READ_PERMISSION = 1;
+    private File imageFile;
+    String ImageString;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Button galleryButton = (Button)findViewById(R.id.Gallery_Button);
-
-        galleryButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                Log.i("Gallery button","User clicked the gallery Button");
-                //gal.loadImagefromGallery();
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                        MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, LOAD_IMAGE);
-            }
-        });
-
-        Button photoButton = (Button)findViewById(R.id.Photo_button);
-        photoButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                Log.i("Photo Button","User clicked the photo Button");
-                setContentView(R.layout.photo_layout);
-
-                //TODO ajout link vers classe Photo.java
-            }
-            });
     }
 
-    protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+    //Fonction pour utiliser l'appareil photo
+
+    public void process(View view){
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imageFile=new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "test.jpg");
+        Uri tempuri=Uri.fromFile(imageFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, tempuri);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        try {
-            // When an Image is picked
-            if (requestCode == LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
 
-                // Get the Image from data
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        if(requestCode==0)
+        {
+            switch (resultCode) {
 
-                // Get the cursor
-                Cursor cursor = getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                // Move to first row
-                cursor.moveToFirst();
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                ImageString = cursor.getString(columnIndex);
-                cursor.close();
-                ImageView imgView = (ImageView) findViewById(R.id.Pictures_ImageView);
-
-                //requests permission to read a files from user's device
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_PERMISSION);
-
-                // Set the Image in ImageView after decoding the String
-                imgView.setImageBitmap(BitmapFactory.decodeFile(ImageString));
-
-            } else {
-                Toast.makeText(this, "Vous n'avez pas selectionné d'images !", Toast.LENGTH_LONG).show();
+                case Activity.RESULT_OK:
+                    if(imageFile.exists())
+                    {
+                        Toast.makeText(this,"The file was saved at "+imageFile.getAbsolutePath(),Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(this,"There was an error saving the file",Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                case Activity.RESULT_CANCELED:
+                    break;
+                default:
+                    break;
             }
-        } catch (Exception e) {
-            Toast.makeText(this, "Problème détecté", Toast.LENGTH_LONG).show();
         }
     }
+
+
+
 }
 
