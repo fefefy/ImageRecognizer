8ea5d8daf4ea07e60e191ac7afbf99b746184294
fefefy <seriousfelix@hotmail.fr>
2016-11-09 15:10:40 +0100

    Merge branch 'master' of C:\Users\serio\Documents\GitHub\ImageRecognizer with conflicts.

--- app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
+++ app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
@@ -1,35 +1,53 @@
 package projetstl.com.imagerecognizer;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class MainActivity extends Activity {
 
+
+    public Gallery gal = new Gallery();
     private static int LOAD_IMAGE = 1;
-    String ImageString;
+    private String ImageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button libraryButton = (Button)findViewById(R.id.Library_Button);
-        libraryButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                Log.i("Library button","User clicked the library Button");
-                setContentView(R.layout.library_layout);
-                //TODO ajout link vers classe Library.java
-            }
+
+        Button galleryButton = (Button)findViewById(R.id.Gallery_Button);
+
+        galleryButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Log.i("Gallery button","User clicked the gallery Button");
+                //setContentView(R.layout.gallery_layout);
+
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                String test = gal.getImageString();
+
+
+
+               }
         });
 
         Button photoButton = (Button)findViewById(R.id.Photo_button);
@@ -43,49 +61,6 @@ public class MainActivity extends Activity {
             });
     }
 
-    //Function to load images from Gallery
-    public void loadImagefromGallery(View view) {
-
-        // Create intent to Open Image applications such as Gallery
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        // Starting Intent
-        startActivityForResult(galleryIntent, LOAD_IMAGE);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            // When an Image is picked
-            if (requestCode == LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-
-                // Get the Image from data
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-                // Get the cursor
-                Cursor cursor = getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                // Move to first row
-                cursor.moveToFirst();
-
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                ImageString = cursor.getString(columnIndex);
-                cursor.close();
-                ImageView imgView = (ImageView) findViewById(R.id.Library_ImageView);
-                // Set the Image in ImageView after decoding the String
-                imgView.setImageBitmap(BitmapFactory.decodeFile(ImageString));
-
-            } else {
-                Toast.makeText(this, "Vous n'avez pas selectionné d'images !", Toast.LENGTH_LONG).show();
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Problème détecté", Toast.LENGTH_LONG).show();
-        }
-
-    }
 
 }
 
