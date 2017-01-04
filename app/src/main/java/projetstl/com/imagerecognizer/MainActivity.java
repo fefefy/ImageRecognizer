e18f0e348c6e876a1a2ea88d9aabedc60c7ced6b
fefefy <seriousfelix@hotmail.fr>
2017-01-04 22:47:10 +0100

    Regroup all code in main activity + Adding Matching class that need to be continued and fixed

--- app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
+++ app/src/main/java/projetstl/com/imagerecognizer/MainActivity.java
@@ -1,36 +1,133 @@
 package projetstl.com.imagerecognizer;
 
+        import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
+
+    private File imageFile;
+    private String CurrentPhotoPath;
+    private static int LOAD_IMAGE = 1;
+    private String ImageString;
+    private int READ_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button galleryButton = (Button)findViewById(R.id.Gallery_Button);
-        galleryButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                Log.i("Gallery button","User clicked the gallery Button");
-                Intent intent = new Intent(MainActivity.this,Gallery.class);
-                startActivity(intent);
-            }
-        });
+    //Function to use Camera
+    public void TakePicture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imageFile = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "ImageRecognizer.jpg");
+        Uri temp_uri = Uri.fromFile(imageFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, temp_uri);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        startActivityForResult(intent, 0);
+    }
 
-        Button photoButton = (Button)findViewById(R.id.Photo_button);
-        photoButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                Log.i("Photo Button","User clicked the photo Button");
-                Intent intent = new Intent(MainActivity.this, Photo.class);
-                startActivity(intent);
+    //Function to Add taken image to gallery
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File imageFile = new File(CurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(imageFile);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    public void loadImageFromGallery(View view) {
+
+        ImageView imgView = (ImageView) findViewById(R.id.Pictures_ImageView);
+
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, LOAD_IMAGE);
+        System.out.println("Gallery ouverte");
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == 0) {
+                switch (resultCode) {
+
+                    case Activity.RESULT_OK:
+                        if (imageFile.exists()) {
+                            //Get absolute path from image
+                            CurrentPhotoPath = imageFile.getAbsolutePath();
+                            Toast.makeText(this, "The file was saved at " + CurrentPhotoPath, Toast.LENGTH_LONG).show();
+                            //return to main activity layout
+                            setContentView(R.layout.activity_main);
+                            ImageView imgView = (ImageView) findViewById(R.id.Pictures_ImageView);
+                            imgView.setImageBitmap(BitmapFactory.decodeFile(String.valueOf(imageFile)));
+                            //Add image to gallery
+                            galleryAddPic();
+                        } else {
+                            Toast.makeText(this, "There was an error saving the file", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                    default:
+                        break;
+                }
             }
-            });
+
+        } catch (Exception e) {
+            Toast.makeText(this, "Problème détecté", Toast.LENGTH_LONG).show();
+        }
+
+        try {
+            // When an Image is picked
+            if (requestCode == LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+
+
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                ImageString = cursor.getString(columnIndex);
+                cursor.close();
+                setContentView(R.layout.activity_main);
+                ImageView imgView = (ImageView) findViewById(R.id.Pictures_ImageView);
+
+                //requests permission to read a files from user's device
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_PERMISSION);
+
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory.decodeFile(ImageString));
+
+            } else if (requestCode != LOAD_IMAGE && resultCode == RESULT_OK){
+                Toast.makeText(this, "Voici votre photo", Toast.LENGTH_LONG).show();
+                    }
+                        else{
+                Toast.makeText(this, "Vous n'avez pas selectionné d'images !", Toast.LENGTH_LONG).show();
+                 }
+        } catch (Exception e) {
+            Toast.makeText(this, "Problème détecté", Toast.LENGTH_LONG).show();
+        }
     }
 }
 
--- app/src/main/java/projetstl/com/imagerecognizer/Matching.java
+++ app/src/main/java/projetstl/com/imagerecognizer/Matching.java
@@ -1,9 +1,72 @@
 package projetstl.com.imagerecognizer;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_features2d;
+import org.bytedeco.javacpp.opencv_features2d.DescriptorExtractor;
+import org.bytedeco.javacpp.opencv_features2d.KeyPoint;
+import org.bytedeco.javacpp.opencv_nonfree.SIFT;
+
+import static org.bytedeco.javacpp.opencv_features2d.drawKeypoints;
+import static org.bytedeco.javacpp.opencv_highgui.imread;
+import static org.bytedeco.javacpp.opencv_highgui.waitKey;
 
 
-public class Matching {
+public class Matching extends Activity{
+
+    private static int LOAD_IMAGE = 1;
+    private String ImageString;
+    private int READ_PERMISSION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(Matching_view);
+
+    }
 
 
+    protected void processing() {
+
+        Mat mImage = imread("data/church01.jpg", 1);
+
+        int nFeatures = 0;
+        int nOctaveLayers = 3;
+        double contrastThreshold = 0.03;
+        int edgeThreshold = 10;
+        double sigma = 1.6;
+
+        // Create Surf Keypoint Detector
+        SIFT siftFeatureDetector = new SIFT(nFeatures, nOctaveLayers, contrastThreshold, edgeThreshold, sigma);
+
+        // Create Surf Extractor
+        DescriptorExtractor siftDescriptorExtractor = DescriptorExtractor.create("SIFT");
+
+        KeyPoint keypoints = new KeyPoint();
+
+        Mat descriptors = new Mat();
+
+        // Read the image
+
+
+        // opencv_imgproc.cvtColor(mImage, mImage, opencv_imgproc.COLOR_BGR2GRAY);
+
+        // Process it
+        siftFeatureDetector.detect(mImage, keypoints);
+
+        siftDescriptorExtractor.compute(mImage, keypoints, descriptors);
+
+        Mat outImage = new opencv_core.Mat();
+        drawKeypoints(mImage, keypoints, outImage, new opencv_core.Scalar(0, 255, 255, 0), opencv_features2d.DrawMatchesFlags.DRAW_RICH_KEYPOINTS);
+
+        //show(outImage, "out");
+
+
+        waitKey(0);
+
+    }
 
 }
